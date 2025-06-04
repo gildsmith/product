000c414ac84c05dbacebee3a3ba998971b4e93d8
Matt Chmielewski <38378328+itsmattch@users.noreply.github.com>
2025-06-05 00:41:34 +0200

    Refactor facade to use static model methods

--- src/Facades/Product.php
+++ src/Facades/Product.php
@@ -12,41 +12,87 @@ class Product implements ProductFacadeInterface
 {
     public function find(string $code, bool $withTrashed = false): ?ProductInterface
     {
-        // TODO: Implement find() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        $query = $withTrashed && method_exists($model, 'getDeletedAtColumn')
+            ? $model::withTrashed()
+            : $model::query();
+
+        return $query->where('code', $code)->first();
     }
 
     public function all(bool $withTrashed = false): Collection
     {
-        // TODO: Implement all() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        $query = $withTrashed && method_exists($model, 'getDeletedAtColumn')
+            ? $model::withTrashed()
+            : $model::query();
+
+        return $query->get();
     }
 
     public function trashed(): Collection
     {
-        // TODO: Implement trashed() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        return method_exists($model, 'onlyTrashed')
+            ? $model::onlyTrashed()->get()
+            : collect();
     }
 
     public function create(array $data): ProductInterface
     {
-        // TODO: Implement create() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        return $model::create($data);
     }
 
     public function update(string $code, array $data): ProductInterface
     {
-        // TODO: Implement update() method.
+        $product = $this->find($code, true);
+
+        if (! $product) {
+            throw new \InvalidArgumentException("Product with code {$code} not found.");
+        }
+
+        $product->update($data);
+
+        return $product->fresh();
     }
 
     public function updateOrCreate(string $code, array $data): ProductInterface
     {
-        // TODO: Implement updateOrCreate() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        return $model::updateOrCreate(['code' => $code], $data);
     }
 
     public function delete(string $code, bool $force = false): bool
     {
-        // TODO: Implement delete() method.
+        /** @var ProductInterface $model */
+        $model = resolve(ProductInterface::class);
+
+        $query = $model::where('code', $code);
+
+        return $force
+            ? (bool) $query->forceDelete()
+            : (bool) $query->delete();
     }
 
     public function restore(string $code): bool
     {
-        // TODO: Implement restore() method.
+        $product = $this->find($code, true);
+
+        if (! $product || ! method_exists($product, 'restore')) {
+            return false;
+        }
+
+        return (bool) $product->restore();
     }
 }
